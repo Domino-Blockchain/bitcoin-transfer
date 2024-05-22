c8d0b8d9a728a20b9bf634e1d8c1444f6fd9ffe3
Sviatoslav Alekseev <sviatoslav.alekseev@redsteep.com>
2024-05-22 09:13:09 -0400

    Add Google KMS sign and get_pubkey

--- multisig_scripts/kms_sign/src/lib.rs
+++ multisig_scripts/kms_sign/src/lib.rs
@@ -1,6 +1,10 @@
 use std::fs::read_to_string;
 
 use aws_sdk_kms as kms;
+use base64::prelude::*;
+use gcloud_sdk::google::cloud::kms::v1::key_management_service_client::KeyManagementServiceClient;
+use gcloud_sdk::google::cloud::kms::v1::{AsymmetricSignRequest, GetPublicKeyRequest};
+use gcloud_sdk::{GoogleApi, GoogleAuthMiddleware};
 use kms::{primitives::Blob, types::SigningAlgorithmSpec};
 
 pub fn load_dotenv() -> Option<()> {
@@ -12,7 +16,9 @@ pub fn load_dotenv() -> Option<()> {
 }
 
 fn load_aws_config(file_name: &str, key_prefix: Option<&str>) -> Option<()> {
-    let credentials = read_to_string(home::home_dir()?.join(format!(".aws/{file_name}"))).ok()?;
+    let credentials_path = home::home_dir()?.join(".aws").join(file_name);
+    let credentials = read_to_string(credentials_path).ok()?;
+
     let value: toml::Table = credentials.parse().ok()?;
     let default = value.get("default")?.as_table()?;
     default.into_iter().for_each(|(k, v)| {
@@ -29,7 +35,34 @@ fn load_aws_config(file_name: &str, key_prefix: Option<&str>) -> Option<()> {
     Some(())
 }
 
-pub async fn sign(digest: Vec<u8>) -> Result<Vec<u8>, kms::Error> {
+pub async fn sign(digest: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    if std::env::var("KEY_ARN").is_ok() {
+        sign_aws(digest).await.map_err(|e| e.into())
+    } else {
+        sign_google(digest).await
+    }
+}
+
+fn debug_sign_big_numbers(signature: &[u8]) {
+    let result: asn1::ParseResult<_> = asn1::parse(signature, |d| {
+        return d.read_element::<asn1::Sequence>()?.parse(|d| {
+            let a = d.read_element::<asn1::BigInt>().unwrap();
+            let b = d.read_element::<asn1::BigInt>().unwrap();
+            assert!(d.is_empty());
+            return Ok((a, b));
+        });
+    });
+    let (a, b) = result.unwrap();
+    eprintln!(
+        "[{file}:{line}] a={:?} b={:?}",
+        a,
+        b,
+        file = file!(),
+        line = line!()
+    );
+}
+
+pub async fn sign_aws(digest: Vec<u8>) -> Result<Vec<u8>, kms::Error> {
     let key_arn = std::env::var("KEY_ARN").unwrap();
 
     let config = aws_config::load_from_env().await;
@@ -47,22 +80,7 @@ pub async fn sign(digest: Vec<u8>) -> Result<Vec<u8>, kms::Error> {
 
     let debug_big_numbers = false;
     if debug_big_numbers {
-        let result: asn1::ParseResult<_> = asn1::parse(&signature, |d| {
-            return d.read_element::<asn1::Sequence>()?.parse(|d| {
-                let a = d.read_element::<asn1::BigInt>().unwrap();
-                let b = d.read_element::<asn1::BigInt>().unwrap();
-                assert!(d.is_empty());
-                return Ok((a, b));
-            });
-        });
-        let (a, b) = result.unwrap();
-        eprintln!(
-            "[{file}:{line}] a={:?} b={:?}",
-            a,
-            b,
-            file = file!(),
-            line = line!()
-        );
+        debug_sign_big_numbers(&signature);
     }
 
     Ok(signature)
@@ -86,7 +104,15 @@ pub fn parse_asn_pubkey<'a>(pk: &'a [u8]) -> asn1::ParseResult<&'a [u8]> {
     .map(|bit_string| bit_string.as_bytes())
 }
 
-pub async fn get_pubkey() -> Result<Vec<u8>, kms::Error> {
+pub async fn get_pubkey() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    if std::env::var("KEY_ARN").is_ok() {
+        get_pubkey_aws().await.map_err(|e| e.into())
+    } else {
+        get_pubkey_google().await
+    }
+}
+
+pub async fn get_pubkey_aws() -> Result<Vec<u8>, kms::Error> {
     let key_arn = std::env::var("KEY_ARN").unwrap();
 
     let config = aws_config::load_from_env().await;
@@ -97,9 +123,70 @@ pub async fn get_pubkey() -> Result<Vec<u8>, kms::Error> {
         .set_key_id(Some(key_arn))
         .send()
         .await?;
-    let pk = res.public_key.unwrap().into_inner();
+    let pk_pem = res.public_key.unwrap().into_inner();
 
-    let pk = parse_asn_pubkey(&pk).unwrap().to_vec();
+    let pk = parse_asn_pubkey(&pk_pem).unwrap().to_vec();
+    Ok(pk)
+}
+
+pub async fn google_kms_client(
+) -> Result<GoogleApi<KeyManagementServiceClient<GoogleAuthMiddleware>>, Box<dyn std::error::Error>>
+{
+    let kms_client: GoogleApi<KeyManagementServiceClient<GoogleAuthMiddleware>> =
+        GoogleApi::from_function(
+            KeyManagementServiceClient::new,
+            "https://cloudkms.googleapis.com",
+            // cloud resource prefix: used only for some of the APIs (such as Firestore)
+            None,
+        )
+        .await?;
+    Ok(kms_client)
+}
+
+// Source: https://github.com/abdolence/gcloud-sdk-rs/blob/master/examples/secrets-manager-client/src/main.rs
+pub async fn sign_google(digest: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let key_name = std::env::var("KEY_NAME")?;
+    let kms_client = google_kms_client().await?;
+
+    let response = kms_client
+        .get()
+        .asymmetric_sign(AsymmetricSignRequest {
+            name: key_name,
+            data: digest,
+            ..Default::default()
+        })
+        .await?;
+    let signature = response.into_inner().signature;
+
+    let debug_big_numbers = false;
+    if debug_big_numbers {
+        debug_sign_big_numbers(&signature);
+    }
+
+    // println!("Response: {:?}", response);
+    Ok(signature)
+}
+
+pub async fn get_pubkey_google() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let key_name = std::env::var("KEY_NAME")?;
+
+    let kms_client = google_kms_client().await?;
+
+    let response = kms_client
+        .get()
+        .get_public_key(GetPublicKeyRequest { name: key_name })
+        .await?;
+
+    let pk_base64 = response.into_inner().pem;
+    let pk_base64 = pk_base64
+        .strip_prefix("-----BEGIN PUBLIC KEY-----\n")
+        .unwrap()
+        .strip_suffix("\n-----END PUBLIC KEY-----\n")
+        .unwrap()
+        .replace('\n', "");
+    let pk_pem = BASE64_STANDARD.decode(&pk_base64).unwrap();
+    let pk = parse_asn_pubkey(&pk_pem).unwrap().to_vec();
+
     Ok(pk)
 }
 
--- src/bin/kms_sign.rs
+++ src/bin/kms_sign.rs
@@ -1,6 +1,6 @@
 use aws_sdk_kms as kms;
 use clap::{arg, command, Command};
-use kms_sign::{get_pubkey, init, sign};
+use kms_sign::{get_pubkey_aws, get_pubkey_google, init, sign_aws, sign_google};
 
 #[tokio::main]
 async fn main() -> Result<(), kms::Error> {
@@ -14,18 +14,33 @@ async fn main() -> Result<(), kms::Error> {
                 .about("Sign with KMS key")
                 .arg(arg!(<BASE64_STR>)),
         )
+        .subcommand(Command::new("get_pubkey_google").about("Get KMS pubkey"))
+        .subcommand(
+            Command::new("sign_google")
+                .about("Sign with KMS key")
+                .arg(arg!(<BASE64_STR>)),
+        )
         .get_matches();
 
     init();
 
     match matches.subcommand() {
-        Some(("get_pubkey", _sub_matches)) => {
-            let pk = get_pubkey().await.unwrap();
+        Some(("get_pubkey_aws", _sub_matches)) => {
+            let pk = get_pubkey_aws().await.unwrap();
             println!("{}", hex::encode(&pk));
         }
-        Some(("sign", sub_matches)) => {
+        Some(("sign_aws", sub_matches)) => {
             let hex_str = sub_matches.get_one::<String>("BASE64_STR").unwrap();
-            let signature = sign(hex::decode(hex_str).unwrap()).await.unwrap();
+            let signature = sign_aws(hex::decode(hex_str).unwrap()).await.unwrap();
+            println!("{}", hex::encode(&signature));
+        }
+        Some(("get_pubkey_google", _sub_matches)) => {
+            let pk = get_pubkey_google().await.unwrap();
+            println!("{}", hex::encode(&pk));
+        }
+        Some(("sign_google", sub_matches)) => {
+            let hex_str = sub_matches.get_one::<String>("BASE64_STR").unwrap();
+            let signature = sign_google(hex::decode(hex_str).unwrap()).await.unwrap();
             println!("{}", hex::encode(&signature));
         }
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
