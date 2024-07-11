281c564e22a3474f872b43faedc24e0eed19bcd5
Sviatoslav Alekseev <sviatoslav.alekseev@redsteep.com>
2024-07-11 08:51:55 -0400

    Add debug logs on spl-token-cli failure

--- src/spl_token.rs
+++ src/spl_token.rs
@@ -6,49 +6,39 @@ pub fn spl_token(args: &[&str]) -> serde_json::Value {
     let cli_path = std::env::var("SPL_TOKEN_CLI_PATH").unwrap();
     let domichain_rpc_url = std::env::var("DOMICHAIN_RPC_URL").unwrap();
     let spl_token_program_id = std::env::var("SPL_TOKEN_PROGRAM_ID").unwrap();
+
+    let mut full_args = vec![
+        "--output",
+        "json",
+        "--url",
+        &domichain_rpc_url,
+        "--program-id",
+        &spl_token_program_id,
+    ];
+    full_args.extend_from_slice(args);
+
     let mut c = std::process::Command::new(&cli_path);
     let command = c
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .arg("--output")
-        .arg("json")
-        .arg("--url")
-        .arg(&domichain_rpc_url)
-        .arg("--program-id")
-        .arg(&spl_token_program_id)
-        .args(args);
+        .args(&full_args);
     let o = command.spawn().unwrap().wait_with_output().unwrap();
+    if !o.status.success() {
+        eprintln!("exec: {cli_path}");
+        for arg in &full_args {
+            eprintln!("    {arg}");
+        }
+
+        eprintln!("status = {}", &o.status);
+    }
     let stdout = o.stdout;
     let stderr = o.stderr;
-    // if !stdout.is_empty() {
-    //     println!("stdout = {}", String::from_utf8_lossy(&stdout));
-    // }
+    if !stdout.is_empty() {
+        eprintln!("stdout = {}", String::from_utf8_lossy(&stdout));
+    }
     if !stderr.is_empty() {
-        eprintln!("args = {args:?}");
         let stderr = String::from_utf8_lossy(&stderr);
         eprintln!("stderr = {stderr}");
-        // let stderr = stderr
-        //     .trim()
-        //     .strip_prefix("Error: Client(Error ")
-        //     .unwrap()
-        //     .strip_suffix(")")
-        //     .unwrap();
-        // println!("stderr = {stderr:?}");
-        // let options = Options::default().with_default_extension(Extensions::EXPLICIT_STRUCT_NAMES);
-        // let stderr: ron::Value = match options.from_str(&stderr) {
-        //     Ok(val) => val,
-        //     Err(err) => {
-        //         stderr.lines().for_each(|line| {
-        //             dbg!(line.get(err.position.col - 10..err.position.col + 10));
-        //         });
-        //         dbg!(&err);
-        //         panic!("ERR: {err:?}");
-        //     }
-        // };
-        // println!(
-        //     "stderr = {}",
-        //     ron::ser::to_string_pretty(&stderr, ron::ser::PrettyConfig::default()).unwrap(),
-        // );
     }
     serde_json::Value::from_str(std::str::from_utf8(&stdout).unwrap()).unwrap()
 }
